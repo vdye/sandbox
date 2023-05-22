5705cfab6643dbc0e40281f6d8e5ded3733b7184
Victoria Dye <vdye@github.com>
2023-05-22 11:05:00 -0700

    Basic working Lambert, slightly incorrect Phong

--- threejs-demo/customMaterial.js
+++ threejs-demo/customMaterial.js
@@ -57,32 +57,87 @@ export const ExampleShaderMaterial = new THREE.ShaderMaterial( {
 } );
 
 const texVertexShader = `
+  varying vec3 vNormal;
   varying vec2 vUv;
+  varying vec3 vViewPosition;
 
   void main() {
-    vUv = uv;
+    vec4 viewModelPosition = modelViewMatrix * vec4(position, 1.0);
 
-    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+    // Pass varyings to fragment shader
+    vViewPosition = viewModelPosition.xyz;
+    vUv = uv;
+    gl_Position = projectionMatrix * viewModelPosition;
+
+    // Rotate the object normals by a 3x3 normal matrix.
+    // We could also do this CPU-side to avoid doing it per-vertex
+    mat3 normalMatrix = transpose(inverse(mat3(modelViewMatrix)));
+    vNormal = normalize(normalMatrix * normal);
   }
 `;
 
 const texFragmentShader = `
+  // Lighting info
+  struct DirectionalLight {
+    vec3 direction;
+    vec3 color;
+  };
+  uniform DirectionalLight directionalLights[ NUM_DIR_LIGHTS ];
+
+  // Phong constants
+  uniform vec3 ambientColor;
+  uniform vec3 specularColor;
+  uniform float Ka;         // Ambient reflection coefficient
+  uniform float Kd;         // Diffuse reflection coefficient
+  uniform float Ks;         // Specular reflection coefficient
+  uniform float shininess;  // Shininess
+
+  // Texture
   uniform sampler2D tex;
+
+  varying vec3 vNormal;
   varying vec2 vUv;
+  varying vec3 vViewPosition;
 
   void main() {
     float newU = abs(vUv.x * 2.0 - 1.0);
-    gl_FragColor = texture2D(tex, vec2(newU, vUv.y));
+    vec4 baseColor = texture2D(tex, vec2(newU, vUv.y));
+
+    // Get the lambert (diffuse) color
+    float diffuseCoeff = max(dot(vNormal, normalize(directionalLights[0].direction)), 0.0);
+    vec3 lambertColor = baseColor.xyz * diffuseCoeff;
+
+    // Get the specular factor
+    vec3 R = reflect(-directionalLights[0].direction, vNormal);
+    float specular = pow(max(dot(-normalize(vViewPosition), R), 0.0), shininess);
+
+    gl_FragColor = vec4(Ka * ambientColor +
+                        Kd * lambertColor +
+                        Ks * specular * specularColor, 1.0);
   }
 `;
 
 const texLoader = new THREE.TextureLoader();
 export const ExampleTextureMaterial = new THREE.ShaderMaterial( {
 
-  uniforms: {
-    tex: { type: "t", value: texLoader.load( "resources/images/ball_texture.png" ) }
-  },
+  lights: true,
+  uniforms: THREE.UniformsUtils.merge([
+    THREE.UniformsLib['lights'],
+    {
+      tex: { type: "t", value: texLoader.load( "resources/images/ball_texture.png" ) }
+    },
+    {
+      ambientColor: {type: 'vec3', value: new THREE.Color(0x000000)},
+      specularColor: {type: 'vec3', value: new THREE.Color(0xFFFFFF)},
+      Ka: { value: 1.0 },
+      Kd: { value: 1.0 },
+      Ks: { value: 1.0 },
+      shininess: { value: 5.0 },
+    }
+  ]),
 
   vertexShader: texVertexShader,
   fragmentShader: texFragmentShader
 } );
+
+console.log(ExampleTextureMaterial);
